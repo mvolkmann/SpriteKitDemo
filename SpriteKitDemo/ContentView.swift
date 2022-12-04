adfb44bc950b53219e394a5206177f53ac0de515
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-04 08:53:46 -0600

    initial commit

--- SpriteKitDemo/ContentView.swift
+++ SpriteKitDemo/ContentView.swift
@@ -1,21 +1,51 @@
-//
-//  ContentView.swift
-//  SpriteKitDemo
-//
-//  Created by Mark Volkmann on 12/4/22.
-//
-
+import SpriteKit
 import SwiftUI
 
+class GameScene: SKScene {
+    let boxSize = 50
+
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    private func randomColor() -> UIColor {
+        UIColor(
+            red: .random(in: 0 ... 1),
+            green: .random(in: 0 ... 1),
+            blue: .random(in: 0 ... 1),
+            alpha: 1
+        )
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let size = CGSize(width: boxSize, height: boxSize)
+        // let box = SKSpriteNode(color: .red, size: size)
+        let box = SKSpriteNode(color: randomColor(), size: size)
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: size)
+        addChild(box)
+    }
+}
+
 struct ContentView: View {
+    let size = CGSize(width: 300, height: 400)
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = size
+        scene.scaleMode = .fill
+        return scene
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Tap to drop a box.").font(.title)
+            SpriteView(scene: scene)
+                .frame(width: size.width, height: size.height)
+                .ignoresSafeArea()
+            Spacer()
         }
-        .padding()
     }
 }
 
