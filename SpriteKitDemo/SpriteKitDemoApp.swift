8c0e5b21412f78d842c2284aa8c0dcc6b519d3de
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-12-04 08:52:52 -0600

    Initial Commit

---
+++ SpriteKitDemo/SpriteKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitDemoApp.swift
+//  SpriteKitDemo
+//
+//  Created by Mark Volkmann on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
